0e7924839cb7746a315b06f840c1d4e904108771
satosis <hung0913003358@gmail.com>
2024-06-15 20:36:45 +0700

    update

--- src/main/java/com/example/watchex/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/watchex/config/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package com.example.watchex.config;
 
+import com.example.watchex.entity.Admin;
 import com.example.watchex.entity.User;
 import com.example.watchex.repository.TokenRepository;
+import com.example.watchex.service.AdminJwtService;
+import com.example.watchex.service.AdminService;
 import com.example.watchex.service.JwtService;
 import com.example.watchex.service.UserService;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -20,8 +23,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Component
@@ -29,7 +33,9 @@ import java.util.Optional;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final AdminJwtService adminJwtService;
     private final UserService userService;
+    private final AdminService adminService;
      private final TokenRepository tokenRepository;
 
     @Override
@@ -38,12 +44,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        Map<String, String[]> parameters = request.getParameterMap();
+        JsonObject jsonObject=new JsonObject();
+        for(String key:parameters.keySet()){
+            jsonObject.addProperty(key, parameters.get(key)[0]);
+        }
+        String type = jsonObject.has("type") ? jsonObject.get("type").getAsString() : null;
         if (request.getServletPath().contains("/api/admin/auth")) {
             filterChain.doFilter(request, response);
             return;
         }
-        HttpSession httpSession = request.getSession();
-        final String authHeader = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQGdtYWlsLmNvbSIsImlhdCI6MTcxODQzOTQ4OSwiZXhwIjoxNzE4NDU3NDg5fQ.ezO1XQEUN2s2dY_vDuysbGuIpX-M2oAEGb8hI8BkqaU";
+        final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
@@ -53,18 +64,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            User user = userService.findByEmail(userEmail);
             var isTokenValid = tokenRepository.findByToken(jwt)
                     .map(t -> !t.isExpired() && !t.isRevoked())
                     .orElse(false);
-            if (jwtService.isTokenValid(jwt, user) && isTokenValid) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                        user,
-                        null,
-                        user.getAuthorities()
-                );
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+            boolean isAdmin = adminService.existsByEmail(userEmail);
+            if (isAdmin) {
+                Admin admin = adminService.findByEmail(userEmail);
+                if (adminJwtService.isTokenValid(jwt, admin) && isTokenValid) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            admin,
+                            null,
+                            admin.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            } else {
+                User user = userService.findByEmail(userEmail);
+                if (jwtService.isTokenValid(jwt, user) && isTokenValid) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                            user,
+                            null,
+                            user.getAuthorities()
+                    );
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
         }
         filterChain.doFilter(request, response);
